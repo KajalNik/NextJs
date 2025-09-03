9be2b98a5ddc978cf17128a6a4ee6825e798fe86
“kajal <kajalnikam.tg@gmail.com>
2025-09-03 06:54:35 +0100

    Implemeted create and read routes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,6 +6,8 @@ import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
 import handleError from "@/lib/handlers/error";
+import { ValidationError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
 import Link from "next/link";
 
 const questions = [
@@ -40,11 +42,12 @@ const questions = [
 ];
 
 const test = async() =>{
-  try{
-    throw new Error("Test Error")
-
-  }
-  catch(error){
+   try {
+      throw new ValidationError({
+      title: ["Required"],
+      tags: ['"JavaScript" is not a valid tag.'],
+    });
+  } catch (error) {
     return handleError(error);
   }
 }
--- lib/handlers/error.ts
+++ lib/handlers/error.ts
@@ -1,30 +1,37 @@
 import { NextResponse } from "next/server";
 import { ZodError } from "zod";
+
 import { RequestError, ValidationError } from "../http-errors";
+import logger from "../logger";
 
 export type ResponseType = "api" | "server";
 
-const formatResponse= (
-    responseType:ResponseType,
-    status: number,
-    message: String,
-    errors?: Record<string, string[]> | undefined
+const formatResponse = (
+  responseType: ResponseType,
+  status: number,
+  message: string,
+  errors?: Record<string, string[]> | undefined
 ) => {
-    const responseContent = {
-        success: false,
-        error: {
-            message,
-            details:errors,
-        },
-    };
+  const responseContent = {
+    success: false,
+    error: {
+      message,
+      details: errors,
+    },
+  };
 
-     return responseType === "api"
+  return responseType === "api"
     ? NextResponse.json(responseContent, { status })
     : { status, ...responseContent };
 };
 
 const handleError = (error: unknown, responseType: ResponseType = "server") => {
   if (error instanceof RequestError) {
+    logger.error(
+      { err: error },
+      `${responseType.toUpperCase()} Error: ${error.message}`
+    );
+
     return formatResponse(
       responseType,
       error.statusCode,
@@ -38,6 +45,11 @@ const handleError = (error: unknown, responseType: ResponseType = "server") => {
       error.flatten().fieldErrors as Record<string, string[]>
     );
 
+    logger.error(
+      { err: error },
+      `Validation Error: ${validationError.message}`
+    );
+
     return formatResponse(
       responseType,
       validationError.statusCode,
@@ -47,9 +59,12 @@ const handleError = (error: unknown, responseType: ResponseType = "server") => {
   }
 
   if (error instanceof Error) {
+    logger.error(error.message);
+
     return formatResponse(responseType, 500, error.message);
   }
 
+  logger.error({ err: error }, "An unexpected error occurred");
   return formatResponse(responseType, 500, "An unexpected error occurred");
 };
 
