6400d7ff5b0c7206fa283958f380d435b85521fe
“kajal <kajalnikam.tg@gmail.com>
2025-09-05 13:56:26 +0100

    sign-up implemetation done

--- auth.ts
+++ auth.ts
@@ -1,16 +1,103 @@
 import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
 import GitHubProvider from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import GoogleProvider from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+import { SignInSchema } from "./lib/validations";
+import { IAccountDoc } from "./database/account.model";
+import { IUserDoc } from "./database/user.model";
+import bcrypt from "bcryptjs";
+import { api } from "./lib/api";
+
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID!,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    GitHub,
+    Google,
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = SignInSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const { data: existingAccount } = (await api.accounts.getByProvider(
+            email
+          )) as ActionResponse<IAccountDoc>;
+
+          if (!existingAccount) return null;
+
+          const { data: existingUser } = (await api.users.getById(
+            existingAccount.userId.toString()
+          )) as ActionResponse<IUserDoc>;
+
+          if (!existingUser) return null;
+
+          const isValidPassword = await bcrypt.compare(
+            password,
+            existingAccount.password!
+          );
+
+          if (isValidPassword) {
+            return {
+              id: existingUser.id,
+              name: existingUser.name,
+              email: existingUser.email,
+              image: existingUser.image,
+            };
+          }
+        }
+        return null;
+      },
     }),
   ],
+  callbacks: {
+    async session({ session, token }) {
+      session.user.id = token.sub as string;
+      return session;
+    },
+    async jwt({ token, account }) {
+      if (account) {
+        const { data: existingAccount, success } =
+          (await api.accounts.getByProvider(
+            account.type === "credentials"
+              ? token.email!
+              : account.providerAccountId
+          )) as ActionResponse<IAccountDoc>;
+
+        if (!success || !existingAccount) return token;
+
+        const userId = existingAccount.userId;
+
+        if (userId) token.sub = userId.toString();
+      }
+
+      return token;
+    },
+    async signIn({ user, profile, account }) {
+      if (account?.type === "credentials") return true;
+      if (!account || !user) return false;
+
+      const userInfo = {
+        name: user.name!,
+        email: user.email!,
+        image: user.image!,
+        username:
+          account.provider === "github"
+            ? (profile?.login as string)
+            : (user.name?.toLowerCase() as string),
+      };
+
+      const { success } = (await api.auth.oAuthSignIn({
+        user: userInfo,
+        provider: account.provider as "github" | "google",
+        providerAccountId: account.providerAccountId,
+      })) as ActionResponse;
+
+      if (!success) return false;
+
+      return true;
+    },
+  },
 });
--- components/forms/AuthForm.tsx
+++ components/forms/AuthForm.tsx
@@ -17,64 +17,100 @@ import {
 import { Input } from "@/components/ui/input"
 import Link from "next/link"
 import ROUTES from "@/constants/routes"
+import { toast } from "sonner"
+import { useRouter } from "next/navigation"
 
 
 interface AuthFormProps<T extends FieldValues> {
-    schema: ZodType<T>;
-    defaultValues: T;
-    onSubmit: (data: T) => Promise<{sucess:boolean}>;
-    formType:"SIGN_IN" | "SIGN-UP";
-  }
-const AuthForm = <T extends FieldValues>  ({
-    schema,formType,defaultValues,onSubmit}: AuthFormProps<T>)=>{
+  schema: ZodType<T>;
+  defaultValues: T;
+  onSubmit: (data: T) => Promise<ActionResponse>;
+  formType: "SIGN_IN" | "SIGN_UP";
+}
+
+const AuthForm = <T extends FieldValues>({
+  schema,
+  defaultValues,
+  formType,
+  onSubmit,
+}: AuthFormProps<T>) => {
+  const router = useRouter();
+
   const form = useForm<z.infer<typeof schema>>({
     resolver: zodResolver(schema),
-    defaultValues: defaultValues as DefaultValues<T>
+    defaultValues: defaultValues as DefaultValues<T>,
   });
 
-const handleSubmit: SubmitHandler<T> = async () =>{};
+  const handleSubmit: SubmitHandler<T> = async (data) => {
+    const result = (await onSubmit(data)) as ActionResponse;
 
-const buttonText = formType === 'SIGN_IN' ? 'Sign In' : 'Sign Up';
+    if (result?.success) {
+      toast({
+        title: "Success",
+        description:
+          formType === "SIGN_IN"
+            ? "Signed in successfully"
+            : "Signed up successfully",
+      });
 
-  return(
+      router.push(ROUTES.HOME);
+    } else {
+      toast({
+        title: `Error ${result?.status}`,
+        description: result?.error?.message,
+        variant: "destructive",
+      });
+    }
+  };
+
+  const buttonText = formType === "SIGN_IN" ? "Sign In" : "Sign Up";
+
+  return (
     <Form {...form}>
-    <form onSubmit={form.handleSubmit(handleSubmit)} className="mt-10 space-y-8">
-      {buttonText}
-      {Object.keys(defaultValues).map((field) =>(
-
-      <FormField
-        key={field}
-        control={form.control}
-        name={field as Path<T>}
-        render={({ field }) => (
-          <FormItem className="flex w-full flex-col gap-2.5">
-            <FormLabel className="paragraph-medium text-dark400_light700">{field.name === "email" ? "Email Address" : field.name.charAt(0).toUpperCase() + field.name.slice(1)}</FormLabel>
-            <FormControl>
-            <Input
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className="mt-10 space-y-6"
+      >
+        {Object.keys(defaultValues).map((field) => (
+          <FormField
+            key={field}
+            control={form.control}
+            name={field as Path<T>}
+            render={({ field }) => (
+              <FormItem className="flex w-full flex-col gap-2.5">
+                <FormLabel className="paragraph-medium text-dark400_light700">
+                  {field.name === "email"
+                    ? "Email Address"
+                    : field.name.charAt(0).toUpperCase() + field.name.slice(1)}
+                </FormLabel>
+                <FormControl>
+                  <Input
                     required
                     type={field.name === "password" ? "password" : "text"}
                     {...field}
                     className="paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 no-focus min-h-12 rounded-1.5 border"
                   />
-            </FormControl>
-            
-            <FormMessage />
-          </FormItem>
-     
-        )}
-      />
-    ))}
-       <Button
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        ))}
+
+        <Button
           disabled={form.formState.isSubmitting}
           className="primary-gradient paragraph-medium min-h-12 w-full rounded-2 px-4 py-3 font-inter !text-light-900"
-        > {form.formState.isSubmitting
-          ? buttonText === "Sign In"
-            ? "Signin In..."
-            : "Signing Up..."
-          : buttonText}</Button>
-           {formType === "SIGN_IN" ? (
+        >
+          {form.formState.isSubmitting
+            ? buttonText === "Sign In"
+              ? "Signin In..."
+              : "Signing Up..."
+            : buttonText}
+        </Button>
+
+        {formType === "SIGN_IN" ? (
           <p>
-            Don't have an account?{" "}
+            Don&apos;t have an account?{" "}
             <Link
               href={ROUTES.SIGN_UP}
               className="paragraph-semibold primary-text-gradient"
@@ -93,9 +129,9 @@ const buttonText = formType === 'SIGN_IN' ? 'Sign In' : 'Sign Up';
             </Link>
           </p>
         )}
-    </form>
-  </Form>
-  )
-}
+      </form>
+    </Form>
+  );
+};
 
 export default AuthForm;
